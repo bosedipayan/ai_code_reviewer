30d33ae4d7918ea3aa974a11baaf972c14f7686c
Dipayan Bose <bosedipayan2000@gmail.com>
2025-02-16 13:36:37 +0530

    setting up frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <main className="main">
+      <div className="left">
+        <div className="code">
+
+        </div>
+        <div className="review">Review</div>
+      </div>
+      <div className="right"></div>
+    </main>
+    </>
+  )
+}
+
+export default App
